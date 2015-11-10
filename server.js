38efb3d0a1c625bf24b2086b78b3e1ca1b6e5d99
Caleb <cawright@ucollege.edu>
2015-11-10 17:25:09 +0000

    started server.js, don't know why its not working

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 // Require the Express module (https://npmjs.com/package/express)
-
+var express = require('express');
 
 // Create a new express application instance by calling `express()`
-
+var app = express();
 
 // Serve files in the 'public' directory with Express's built-in static file server
-
+app.use(express.static('public'));
 
 // Create a Counter class that will be used to create counter objects
 // See the full description in README.md
+var counter = 0;
 
+// function counter(){
+    
+// }
+app.get('/puppies', function(req, res){
+    counter++;
+   res.send('Thank you for voting! Puppies have ' + counter + ' total votes so far.'); 
+});
 
 // Create a new Counter instance, like: `var voteCounter = new Counter()`
 
@@ -29,3 +37,7 @@
 
 
 // Have the Express application listen for incoming requests on port 8080
+
+app.listen(8080);
+
+console.log("Server is up!");
