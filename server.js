26f351bf183a4149b366b35e83d777cf388c654c
bretttrana <brett.trana@gmail.com>
2016-11-07 03:15:33 +0000

    With these upgrade you never stood a chance.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Require the Express module (https://npmjs.com/package/express)
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 // Create a new express application instance by calling `express()`
 var app = express();
@@ -9,7 +11,7 @@ app.use(express.static('public'));
 
 // Create a Counter class that will be used to create counter objects
 // See the full description in README.md
-var Counter = require('./test/counter.js');
+var Counter = require('./counter.js');
 
 // Create a new Counter instance, like: `var voteCounter = new Counter()`
 var counter = new Counter();
@@ -19,21 +21,33 @@ var counter = new Counter();
 // - Retrieve the new cumulative votes for 'kittens'
 // - Respond with with the message:
 //     "Thank you for voting! Kittens have 12 total votes so far."
-app.get('/kittens', function (req, res) {
-    counter.record('kittens');
-    var kscore = counter.retrieve('kittens');
-    res.send("Thank you for voting! Kittens have " + kscore + " total votes so far.");
-})
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser())
 
-// Respond to 'get' requests for the route '/puppies'
-// - Record a vote for 'puppies'
-// - Retrieve the new cumulative votes for 'puppies'
-// - Respond with with the message:
-//     "Thank you for voting! Puppies have 12 total votes so far."
-app.get('/puppies', function (req, res) {
-    counter.record('puppies');
-    var pscore = counter.retrieve('puppies');
-    res.send("Thank you for voting! Puppies have " + pscore + " total votes so far.");
+
+app.post('/vote', function (req, res) {
+    var choice = req.body.choice;
+    var kitVotes = counter.retrieve('kittens');
+    var pupVotes = counter.retrieve('puppies');
+    
+    res.cookie('voted', true);
+    
+    if(req.cookies.voted) {
+        res.render('voted', {
+            pupVotes: pupVotes,
+            kitVotes: kitVotes
+        })
+    }
+    else {
+        counter.record(choice);
+        res.render('vote', {
+            choice: choice,
+            pupVotes: pupVotes,
+            kitVotes: kitVotes
+        })
+    }
+    
 })
 
 // Have the Express application listen for incoming requests on port 8080
