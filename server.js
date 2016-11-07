7f722b28ea24c84f2a8ab05db948db31a02e2e28
jeanchristian96 <tourejeanchristian@hotmail.fr>
2016-11-07 23:06:34 +0000

    Created a casting vote

--- server.js
+++ server.js
@@ -1,31 +1,36 @@
-// Require the Express module (https://npmjs.com/package/express)
-
-
-// Create a new express application instance by calling `express()`
-
-
-// Serve files in the 'public' directory with Express's built-in static file server
-
-
-// Create a Counter class that will be used to create counter objects
-// See the full description in README.md
-
-
-// Create a new Counter instance, like: `var voteCounter = new Counter()`
-
-
-// Respond to 'get' requests for the route '/kittens'
-// - Record a vote for 'kittens'
-// - Retrieve the new cumulative votes for 'kittens'
-// - Respond with with the message:
-//     "Thank you for voting! Kittens have 12 total votes so far."
-
-
-// Respond to 'get' requests for the route '/puppies'
-// - Record a vote for 'puppies'
-// - Retrieve the new cumulative votes for 'puppies'
-// - Respond with with the message:
-//     "Thank you for voting! Puppies have 12 total votes so far."
-
-
-// Have the Express application listen for incoming requests on port 8080
+    var express = require('express');
+    var app = express();
+    app.use(express.static("public"));
+    
+    function Counter() {
+        this.count = {};
+    }
+    Counter.prototype.record = function(key) {
+        if (isNaN(this.count[key])) {
+             this.count[key] = 0;
+        }
+         this.count[key]++;
+    };
+    Counter.prototype.retrieve = function(key) {
+         return this.count[key];
+    };
+    Counter.prototype.results = function() {
+        return this.count;
+    };
+    var voteCounter = new Counter();
+    voteCounter.record('kittens');
+    var kittenVotes = voteCounter.retrieve('kittens');
+    => 1
+    app.get('/kittens', function (req, res) {
+         voteCounter.record('kittens');
+         var score = voteCounter.retrieve('kittens');
+         res.send("Thanks for voting! We really appreciate it! Kittens have " + score + " total votes so far.");
+    });
+    app.get("/puppies", function (req, res) {
+         voteCounter.record("puppies");
+         var score = voteCounter.retrieve("puppies");
+         res.send("Thanks for voting! We really appreciate it! Puppies have " + score + " total votes so far.");
+    });
+    app.listen(8080);
+    console.log("Server is up!");
+         
