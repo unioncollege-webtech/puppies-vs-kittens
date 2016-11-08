0863498a356fe173b22cf3c1f686097424ad990a
jeanchristian96 <tourejeanchristian@hotmail.fr>
2016-11-08 13:46:59 +0000

    Correct some mistakes in server.js

--- server.js
+++ server.js
@@ -1,36 +1,26 @@
-    var express = require('express');
-    var app = express();
-    app.use(express.static("public"));
-    
-    function Counter() {
-        this.count = {};
-    }
-    Counter.prototype.record = function(key) {
-        if (isNaN(this.count[key])) {
-             this.count[key] = 0;
-        }
-         this.count[key]++;
-    };
-    Counter.prototype.retrieve = function(key) {
-         return this.count[key];
-    };
-    Counter.prototype.results = function() {
-        return this.count;
-    };
-    var voteCounter = new Counter();
-    voteCounter.record('kittens');
-    var kittenVotes = voteCounter.retrieve('kittens');
-    => 1
-    app.get('/kittens', function (req, res) {
-         voteCounter.record('kittens');
-         var score = voteCounter.retrieve('kittens');
-         res.send("Thanks for voting! We really appreciate it! Kittens have " + score + " total votes so far.");
-    });
-    app.get("/puppies", function (req, res) {
-         voteCounter.record("puppies");
-         var score = voteCounter.retrieve("puppies");
-         res.send("Thanks for voting! We really appreciate it! Puppies have " + score + " total votes so far.");
-    });
-    app.listen(8080);
-    console.log("Server is up!");
-         
+var express = require('express');
+
+// Create a new application instance
+var app = express();
+
+// Use Express's built-in static file server
+app.use(express.static('public'));
+
+// Also respond to `GET` requests at the path '/hi'
+app.get('/kittens', function(req, res){
+   res.send('Thanks for voting for kittens'); 
+});
+
+// Also respond to `GET` requests at the path '/hi'
+app.get('/puppies', function(req, res){
+   res.send('Thanks for voting for puppies'); 
+
+
+// Listen on port 8080 for Cloud9
+// https://docs.c9.io/docs/run-an-application#section-environment-variables
+app.listen(8080);
+
+//Log a message to the console.
+console.log("Server is up!");
+
+
