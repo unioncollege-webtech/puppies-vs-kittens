1e9c89971fa3dedcf238a7db580554055839d776
jeanchristian96 <tourejeanchristian@hotmail.fr>
2016-11-14 23:21:19 +0000

    I added a count function

--- server.js
+++ server.js
@@ -6,15 +6,41 @@ var app = express();
 // Use Express's built-in static file server
 app.use(express.static('public'));
 
+//Count Class
+
+function Count(){
+      this.haveVotes = {
+         kittens: 0,
+         puppies: 0
+      };
+}
+
+Count.prototype.record = function(key){
+   this.haveVotes[key]++;
+};
+
+Count.prototype.retrieve = function(key){
+   return this.haveVotes[key]++;
+};
+
+Count.prototype.results = function(key){
+   return this.haveVotes;
+};
+// Create a counter instance
+
+var ScoreCounter = new Count();
+
 // Also respond to `GET` requests at the path '/hi'
 app.get('/kittens', function(req, res){
-   res.send('Thanks for voting for kittens'); 
+   ScoreCounter.record('kittens');
+   res.send('Thanks for voting for kittens! Kittens have ' + ScoreCounter.retrieve('kittens') + ' total votes so far.'); 
 });
 
 // Also respond to `GET` requests at the path '/hi'
 app.get('/puppies', function(req, res){
-   res.send('Thanks for voting for puppies'); 
-
+   ScoreCounter.record('puppies');
+   res.send('Thanks for voting for puppies ! Puppies have ' + ScoreCounter.retrieve('puppies') + ' total votes so far.'); 
+});
 
 // Listen on port 8080 for Cloud9
 // https://docs.c9.io/docs/run-an-application#section-environment-variables
